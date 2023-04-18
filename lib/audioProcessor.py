52a78e9dd6c6dda345c924fe5efff913930024d1
sowecthal <sowecthal@gmail.com>
2023-04-18 21:11:28 +0700

    Made some progress with the audio proccessing module

--- lib/audioProcessor.py
+++ lib/audioProcessor.py
@@ -1,3 +1,36 @@
-class AudioProcessor:
-    def __init__(self):
-        pass
+from pydub import AudioSegment, effects
+
+def equalizeFile(path: str, low: int, mid: int, high: int):
+    pass
+
+def compressFile(path: str, threshold: int, ratio: int):
+    pass
+
+def normalizeFile(path: str, multiplier=None, dBFS=None):
+    input_signal = AudioSegment.from_file(path, path.split('.')[-1])
+    print("dBFS before normalization: ", round(input_signal.dBFS, 1))
+
+    if not (multiplier or dBFS):
+        output_signal = effects.normalize(input_signal)
+        print("dBFS after normalization: ", round(output_signal.dBFS, 1))
+
+    else:
+        target_dBFS = dBFS if dBFS else round(input_signal.dBFS, 1) * multiplier
+        delta_dBFS = target_dBFS - input_signal.dBFS
+        output_signal = input_signal.apply_gain(delta_dBFS)
+        print("dBFS after normalization: ", round(output_signal.dBFS, 1))
+
+    output_path = output_path if output_path else path
+    output_signal.export(output_path, format=output_path.split('.')[-1])
+
+def byReference(path: str, ref_path: str):
+    def getReferenceParams(ref_path: str) -> dict:
+        params = {}
+        input_signal = AudioSegment.from_file(ref_path, ref_path.split('.')[-1])
+        params['dBFS'] = round(input_signal.dBFS, 1)
+        return params
+
+    def applyReferenceParams(path: str, params: dict):
+        normalizeFile(path, dBFS=params['dBFS'])
+    
+    applyReferenceParams(path, getReferenceParams(ref_path))
