162fbcaac7586e8b92cea096d20a6945213ef79c
sowecthal <sowecthal@gmail.com>
2023-04-19 23:03:50 +0700

    Bug fix

--- lib/audioProcessor.py
+++ lib/audioProcessor.py
@@ -11,31 +11,29 @@ def equalizeFile(path: str, low_gain: float, mid_gain: float, high_gain: float):
     b_low, a_low = signal.butter(6, low, 'lowpass', fs=rate)
     b_mid, a_mid = signal.butter(6, [low, high], 'bandpass', fs=rate)
     b_high, a_high = signal.butter(6, high, 'highpass', fs=rate)
-    
-    low_signal = signal.filtfilt(b_low, a_low, data)
-    mid_signal = signal.filtfilt(b_mid, a_mid, data)
-    high_signal = signal.filtfilt(b_high, a_high, data)
+
+    low_signal = signal.filtfilt(b_low, a_low, data, padlen=0)
+    mid_signal = signal.filtfilt(b_mid, a_mid, data, padlen=0)
+    high_signal = signal.filtfilt(b_high, a_high, data, padlen=0)
 
     low_signal *= low_gain
     mid_signal *= mid_gain
     high_signal *= high_gain
 
     output_signal = low_signal + mid_signal + high_signal
-    output_signal = np.int16(output_signal / np.max(np.abs(output_signal)) * 32767)
-
-    output_path = output_path if output_path else path
+    #output_signal = np.int16(output_signal / np.max(np.abs(output_signal)) * 32767)
     output_signal = AudioSegment(output_signal.tobytes(), frame_rate=rate, sample_width=2, channels=1)
-    output_signal.export(output_path, format=output_path.split('.')[-1])
 
-def compressFile(path: str, threshold: float, ratio: float, attack=None, release=None):
+    output_signal.export(path, format=path.split('.')[-1])
+
+def compressFile(path: str, threshold: float, ratio: float, attack=5.0, release=50.0):
     input_signal = AudioSegment.from_file(path, path.split('.')[-1])
     print("RMS level before compression: ", input_signal.rms)
 
     output_signal = input_signal.compress_dynamic_range(threshold=threshold, ratio=ratio, attack=attack, release=release)
     print("RMS level after compression: ", output_signal.rms)
     
-    output_path = output_path if output_path else path
-    output_signal.export(output_path, format=output_path.split('.')[-1])
+    output_signal.export(path, format=path.split('.')[-1])
 
 def normalizeFile(path: str, multiplier=None, dBFS=None):
     input_signal = AudioSegment.from_file(path, path.split('.')[-1])
@@ -50,8 +48,7 @@ def normalizeFile(path: str, multiplier=None, dBFS=None):
         output_signal = input_signal.apply_gain(delta_dBFS)
         print("dBFS after normalization: ", round(output_signal.dBFS, 1))
 
-    output_path = output_path if output_path else path
-    output_signal.export(output_path, format=output_path.split('.')[-1])
+    output_signal.export(path, format=path.split('.')[-1])
 
 def byReference(path: str, ref_path: str):
     def getReferenceParams(ref_path: str) -> dict:
